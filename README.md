8603defb93073dd7bb8036f53148d26e9049d8b2
Joe Levering <coyoteejl@gmail.com>
2018-11-20 16:35:15 -0500

    Hardcode behavior triggers

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # TODO
 
-* Research "actions" string alternative for behaviors
-* Hardcode list of triggers for behaviors
 * Add skills to DB? Would make picking them easier, could also make adding classes easier
 * Spot check the DB
--- app/models/behavior.rb
+++ app/models/behavior.rb
@@ -1,4 +1,11 @@
 class Behavior < ActiveRecord::Base
+  POSSIBLE_TRIGGERS = [
+    "pc-enters",
+    "pc-leaves"
+  ]
+
   belongs_to :npc
   has_many :actions, class_name: "BehaviorAction"
+
+  validates_inclusion_of :trigger, in: POSSIBLE_TRIGGERS
 end
